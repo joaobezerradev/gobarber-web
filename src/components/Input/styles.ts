79db0e33e6467818c1107fdb830823fa81675c57
Joao Vitor <vitor.lira98@gmail.com>
2020-10-05 12:24:22 -0300

    Isolando components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #faede8;
+
+  display: flex;
+  align-items: center;
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #faede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+    color: #666360;
+  }
+
+  & + div {
+    margin-top: 8px;
+  }
+`;
