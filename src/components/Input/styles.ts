99d416d4897c744e5f4abf78275d6e935b990ea6
Joao Vitor <vitor.lira98@gmail.com>
2020-10-05 19:29:14 -0300

    Usabilidade do input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,15 +1,34 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  color: #faede8;
+
+  border: 2px solid #232129;
+  color: #666360;
 
   display: flex;
   align-items: center;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
@@ -24,7 +43,6 @@ export const Container = styled.div`
 
   svg {
     margin-right: 16px;
-    color: #666360;
   }
 
   & + div {
