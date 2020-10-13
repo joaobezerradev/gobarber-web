5ed2fdd0d27b6cdf127862556318b2f05ad37516
Joao Vitor <vitor.lira98@gmail.com>
2020-10-12 23:16:06 -0300

    Rotas autenticadas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <h1>Dashboard</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+      totam? Amet alias reiciendis magni quaerat dolorem odio optio, accusamus,
+      rerum eveniet distinctio, magnam similique harum consequuntur esse debitis
+      aliquid porro.
+    </p>
+  </Container>
+);
+
+export default Dashboard;
